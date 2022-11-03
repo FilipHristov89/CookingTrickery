ae476eeaff1e1d14bbf02ad989ccedebac5bee51
FilipHristov89 <99825747+FilipHristov89@users.noreply.github.com>
2022-11-03 20:49:20 +0200

    Add project files.

---
+++ CookingTrickery.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CookingTrickery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
